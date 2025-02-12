0424ffcd48593a8e3dbde099aac893d9e681e917
Diana Zayats <marusya3651@gmail.com>
2025-02-12 21:57:40 +0200

    Add .gitignore file

---
+++ lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationInsightsTelemetry(options =>
+{
+    options.ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"];
+});
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration>(config =>
+{
+    config.SetAzureTokenCredential(new DefaultAzureCredential());
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
